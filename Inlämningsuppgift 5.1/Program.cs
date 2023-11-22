7714c8cab1172c630f164402fa3e46ada3ddc306
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2023-11-22 09:52:22 +0100

    Add project files.

---
+++ Inlämningsuppgift 5.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace uppgift_5._1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv in 5 namn");
+            string[] namn = new string[5];
+            for (int i = 0; i < namn.Length; i++)
+            {
+                namn[i] = Console.ReadLine();
+            }
+            for (int i = 0;i < namn.Length; i++)
+            {
+                Console.WriteLine(namn[i]);
+            }
+            Console.Read();
+        }
+    }
+}
